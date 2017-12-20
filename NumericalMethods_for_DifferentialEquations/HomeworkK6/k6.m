f9fdf9e5b99684f902c92d29b78e71ba705a04e9
IvanFilipov <vanaka1189@gmail.com>
2017-12-20 11:48:35 +0200

    finale code update

--- NumericalMethods_for_DifferentialEquations/HomeworkK6/k6.m
+++ NumericalMethods_for_DifferentialEquations/HomeworkK6/k6.m
@@ -1,22 +1,44 @@
-f='x1+x2'
+f='x1+cos(3*x2)' ; %+x1^2 + 2*x2';
+f_inl = inline(f,'x1', 'x2');
 
-h=1/10;
+N = 10;
 
-eps = 0.00001;
+h=1/N;
+
+eps = 10 ^ (-3);
 
 x=0:h:1;
 n = size(x)(2);
 
-[y] = k6_method(f,h,eps);
-[y1] = k6_method(f,h/2,eps);
-[y2] = k6_method(f,h/4,eps);
+[y] = k6_method(f_inl, h, eps);
 
-alfa = [];
-for i = 2:(n - 1)
-  for j = 2:(n-1)
-    newalfa = log(abs((y(i,j) + y1(2*i-1,2*j-1))/(y1(2*i-1,2*j-1) + y2(2*(i-1) - 1,2*(j-1) - 1))))/log(2);
-    alfa = [alfa newalfa];
-  end
-end
+yH = y(1:n,1:n);
+xxx=0:h:1;
+yyy=0:h:1;
+[xi,yi]=meshgrid(xxx,yyy);
+mesh(xi,yi,y)
 
-alfa'
+
+[y1] = k6_method(f_inl, h/2, eps);
+[y2] = k6_method(f_inl, h/4, eps);
+
+yH2 = y1(1:2:2*n, 1:2:2*n);
+yH4 = y2(1:4:4*n, 1:4:4*n);
+
+alpha = [];
+
+for i = 2: (n - 1)
+  for j = 2: (n - 1)
+    
+    newalpha = log(abs((yH(i, j) + yH2(i, j))/(yH2(i, j) + yH4(i, j))))/log(2);
+    alpha = [alpha newalpha];
+    
+  endfor  
+  
+  %printf("\n");
+  
+endfor
+
+alpha'
+
+
--- NumericalMethods_for_DifferentialEquations/HomeworkK6/k6_method.m
+++ NumericalMethods_for_DifferentialEquations/HomeworkK6/k6_method.m
@@ -1,28 +1,82 @@
-function [y] = k6_method(f, h, eps)
-  f_inline = inline(f, 'x1', 'x2');
+function [y] = k6_method(f_inl, h, eps)
   
   x = 0:h:1;
+  
   n = size(x)(2);
-  y = zeros(n);
-  yold = ones(n);
+
+  y = zeros(n,n);
+  
+  yold = ones(n,n);
+  
+  
+  f = zeros(n , n);
+  %first feval
+  for i = 2 : (n-1)
+      
+      for j = 2 : (n-1)
+        
+        f(i, j) = feval(f_inl, x(i), x(j));
+        
+      endfor
+      
+    endfor
+  
+  
+  k = 0;
   while(!check(y, yold, eps, n))
+  
+    k = k + 1;
     yold = y;
-    for i=2:(n-1)
-      for j = 2:(n-1)
-        fij = feval(f_inline, x(i), x(j));
-        y(i,j) = (y(i-1,j) + y(i+1,j) + y(i, j -1) + y(i, j + 1) + (h*h*fij))/4;
-      end
-    end
-  end
-end
+    
+    for i = 2 : (n-1)
+      
+      for j = 2 : (n-1)
+        
+        y(i,j) = (yold(i-1,j) + yold(i+1,j) + yold(i, j -1) + yold(i, j + 1) + (h*h*f(i, j)))/4;
+        
+      endfor
+      
+    endfor
+    
+    
+  endwhile
+  
+  printf("%d\n",k);
+endfunction
 
 function [res] = check(y, yold, eps, n)
-  res=1;
-  for i=2:(n-1)
-    for j = 2:(n-1)
-      if( abs(y(i,j) - yold(i,j)) >= eps)
-        res = 0;
-      end
-    end
-  end
-end
+  
+%res = false;
+%  
+%  maxY = max(max(y));
+%  
+%  if(maxY == 0)
+%   
+%      return;
+%  
+%  endif
+%  
+%  
+%  maxDiff = max(max( y .- yold ));
+%  
+%  if( abs(maxDiff) / abs(maxY) < eps )
+%        
+%      res = true;  
+%      return;
+%      
+%  endif
+  
+  
+  res = true;
+  for i = 2 : (n-1)
+    for j = 2 : (n-1)
+      
+      if( (abs(y(i, j) - yold(i, j))) >= eps )
+         res = false;
+         return 
+      endif
+      
+    endfor
+  endfor
+  
+endfunction
